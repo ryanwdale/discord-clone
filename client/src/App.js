e3704abd08acdea3c1b08ebef6fe8ad9a2ad4f82
Jonston Chan <jonstonc@sfu.ca>
2021-11-21 11:51:05 -0800

    Update main page to include login form

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Button, Checkbox, Container, Form, Header } from 'semantic-ui-react';
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container style={{ margin: '2em' }}>
+      <Header as='h1'>Welcome to the project for CMPT 470 of group 1</Header>
+
+      <p>
+        You must login to continue.
+      </p>
+
+      <Form>
+        <Form.Field>
+          <label>Username</label>
+          <input placeholder='Username' />
+        </Form.Field>
+        <Form.Field>
+          <label>Password</label>
+          <input placeholder='Password' />
+        </Form.Field>
+        <Form.Field>
+          <Checkbox label='I want to register an account with the information provided above' />
+        </Form.Field>
+        <Button type='submit'>Submit</Button>
+      </Form>
+    </Container>
   );
 }
 
