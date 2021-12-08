c42d8b2eab929aac0306494e9be0a0a2333ceb01
twc10 <twc10@sfu.ca>
2021-12-07 20:09:24 -0800

    merge master

--- client/src/Chat/Chatroom.js
+++ client/src/Chat/Chatroom.js
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import { Header } from "semantic-ui-react";
+import { Header, Button } from "semantic-ui-react";
 import "./chatroom.css";
 import Message from "./Message";
 import Search from "./Search"
@@ -12,41 +12,73 @@ class Chatroom extends Component {
           <div className="headerContainer">
             <Header as="h2">{this.props.channelName}</Header>
           </div>
-          <Search
-            activeSearchMessage={this.props.activeSearchMessage}
-            handleSearchChange={this.props.handleSearchChange}
-            handleSubmitSearchMessage={this.props.handleSubmitSearchMessage}
-          />
+          {this.props.showAnalytics ?
+            <Button className="analytics_btn" onClick={this.props.toggleShowAnalytics}>Back</Button> :
+            <>
+              <Button className="analytics_btn" onClick={this.props.fetchChannelAnalytics}>View Channel Analytics</Button>
+              <Search
+                activeSearchMessage={this.props.activeSearchMessage}
+                handleSearchChange={this.props.handleSearchChange}
+                handleSubmitSearchMessage={this.props.handleSubmitSearchMessage}
+              /> 
+            </>
+          }
         </div>
-        <div id="chatMessages" className="chatMessages">
-          {this.props.messageList.length !== 0 &&
-            this.props.messageList.map((message) => {
-              return (
-                <Message
-                  key={message.id}
-                  channelId={this.props.channelId}
-                  messageId={message.id}
-                  isFromCurrentUser={this.props.activeUserId === message.user_id}
-                  displayName={message.display_name}
-                  timestamp={message.timestamp}
-                  messageContent={message.message_content}
-                  socket={this.props.socket}
+        {this.props.showAnalytics ? 
+          <div className="analytics_div">
+            <Header as="h2">Channel analytics</Header>
+            {this.props.analytics.user_stats && 
+              <div className="analytics_inner_div">
+                  <Header as="h3">Top users and their message counts:</Header>
+                  {this.props.analytics.user_stats.map(user_stat => {
+                    return (
+                      <>
+                        <span className="analytics_text">{user_stat[0]} sent {user_stat[1]} messages</span>
+                        <br/>
+                      </>
+                    )
+                  })}
+              </div>}
+            {this.props.analytics.wordcount_stats && 
+              <div className="analytics_inner_div">
+                  <Header as="h3">Top words used in this channel:</Header>
+                  {this.props.analytics.wordcount_stats.map(wordcount_stat => {
+                    return (
+                      <>
+                        <span className="analytics_text">"{wordcount_stat[0]}" occured {wordcount_stat[1]} times</span>
+                        <br/>
+                      </>
+                    )
+                  })}
+              </div>}
+          </div> :
+          <>
+            <div id="chatMessages" className="chatMessages">
+              {this.props.messageList.length !== 0 &&
+                this.props.messageList.map((message) => {
+                  return (
+                    <Message
+                      key={message.id}
+                      displayName={message.display_name}
+                      timestamp={message.timestamp}
+                      messageContent={message.message_content}
+                    />
+                  );
+                })}
+            </div>
+            <div className="chatInput">
+              <form onSubmit={(e) => this.props.handleSubmitMessage(e)}>
+                <input
+                  placeholder={"Send message to " + this.props.channelName}
+                  onChange={(event) => this.props.handleChange(event.target.value)}
+                  value={this.props.activeMessage}
                 />
-              );
-            })}
-        </div>
-        <div className="chatInput">
-          <form onSubmit={(e) => this.props.handleSubmitMessage(e)}>
-            <input
-              placeholder={"Send message to " + this.props.channelName}
-              onChange={(event) => this.props.handleChange(event.target.value)}
-              value={this.props.activeMessage}
-            />
-            <button type="submit" className="chatInputSubmit">
-              send message
-            </button>
-          </form>
-        </div>
+                <button type="submit" className="chatInputSubmit">
+                  send message
+                </button>
+              </form>
+            </div>
+          </>}
       </div>
     );
   }
