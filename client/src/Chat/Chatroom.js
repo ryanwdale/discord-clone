dadcd457d9f5008b46361e9a56bbc37919468d19
Jonston Chan <jonstonc@sfu.ca>
2021-12-09 09:49:26 -0800

    Add front end for text search

--- client/src/Chat/Chatroom.js
+++ client/src/Chat/Chatroom.js
@@ -1,97 +1,171 @@
-import { Component } from "react";
-import { Header, Button, Icon } from "semantic-ui-react";
+import axios from "axios";
+import { Component, useEffect, useState } from "react";
+import { Button, Header, Icon } from "semantic-ui-react";
+
 import "./chatroom.css";
 import Message from "./Message";
-import Search from "./Search"
+import Search from "./Search";
 
-class Chatroom extends Component {
-  render() {
-    return (
-      <div className="chatroom">
-        <div className="channelTopBar">
-          <div className="headerContainer">
-            <Header as="h2">{this.props.channelName}</Header>
-          </div>
-          {this.props.showAnalytics ?
-            <Button className="analytics_btn" onClick={this.props.toggleShowAnalytics}>Back</Button> :
-            <>
-              {this.props.channelId !== null && 
-                <Icon
-                  className="trash-button"
-                  name="trash alternate outline"
-                  onClick={this.props.deleteChannel}
-                />
-              }
-              <Button className="analytics_btn" onClick={this.props.fetchChannelAnalytics}>View Channel Analytics</Button>
-              <Search
-                activeSearchMessage={this.props.activeSearchMessage}
-                handleSearchChange={this.props.handleSearchChange}
-                handleSubmitSearchMessage={this.props.handleSubmitSearchMessage}
-              /> 
-            </>
-          }
+const Chatroom = (props) => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [activeSearchQuery, setActiveSearchQuery] = useState("");
+  const [searchResults, setSearchResults] = useState(null);
+
+  const handleSearchChange = (query) =>
+    query === "" ? resetSearch() : setSearchQuery(query);
+
+  const handleSubmitSearchMessage = (e) => {
+    e.preventDefault();
+
+    if (searchQuery === "") {
+      return;
+    }
+
+    setActiveSearchQuery(searchQuery);
+
+    axios
+      .get(`/api/channels/${props.channelId}/messages/search`, {
+        params: {
+          query: searchQuery,
+        },
+      })
+      .then((res) => {
+        setSearchResults(res.data);
+      });
+  };
+
+  const resetSearch = () => {
+    setSearchQuery(""), setActiveSearchQuery(""), setSearchResults(null);
+  };
+  const disabledSearchMessage =
+    "Chat is disabled in search mode. Clear search query to continue chatting";
+  useEffect(resetSearch, [props.channelId]);
+
+  return (
+    <div className="chatroom">
+      <div className="channelTopBar">
+        <div className="headerContainer">
+          <Header as="h2">{props.channelName}</Header>
         </div>
-        {this.props.showAnalytics ? 
-          <div className="analytics_div">
-            <Header as="h3">Channel analytics</Header>
-            {this.props.analytics.user_stats && 
-              <div className="analytics_inner_div">
-                  <Header as="h3">Top users and their message counts:</Header>
-                  {this.props.analytics.user_stats.map(user_stat => {
-                    return (
-                      <>
-                        <span key={user_stat[0]} className="analytics_text">{user_stat[0]} sent {user_stat[1]} messages</span>
-                        <br/>
-                      </>
-                    )
-                  })}
-              </div>}
-            {this.props.analytics.wordcount_stats && 
-              <div className="analytics_inner_div">
-                  <Header as="h3">Top words used in this channel:</Header>
-                  {this.props.analytics.wordcount_stats.map(wordcount_stat => {
-                    return (
-                      <>
-                        <span key={wordcount_stat[0]} className="analytics_text">"{wordcount_stat[0]}" occurred {wordcount_stat[1]} times</span>
-                        <br/>
-                      </>
-                    )
-                  })}
-              </div>}
-          </div> :
+        {props.showAnalytics ? (
+          <Button className="analytics_btn" onClick={props.toggleShowAnalytics}>
+            Back
+          </Button>
+        ) : (
           <>
-            <div id="chatMessages" className="chatMessages">
-              {this.props.messageList.length !== 0 &&
-                this.props.messageList.map((message) => {
+            {props.channelId !== null && (
+              <Icon
+                className="trash-button"
+                name="trash alternate outline"
+                onClick={props.deleteChannel}
+              />
+            )}
+            <Button
+              className="analytics_btn"
+              onClick={props.fetchChannelAnalytics}
+            >
+              View Channel Analytics
+            </Button>
+            <Search
+              activeSearchMessage={searchQuery}
+              handleSearchChange={handleSearchChange}
+              handleSubmitSearchMessage={handleSubmitSearchMessage}
+            />
+          </>
+        )}
+      </div>
+      {props.showAnalytics ? (
+        <div className="analytics_div">
+          <Header as="h3">Channel analytics</Header>
+          {props.analytics.user_stats && (
+            <div className="analytics_inner_div">
+              <Header as="h3">Top users and their message counts:</Header>
+              {props.analytics.user_stats.map((user_stat) => {
+                return (
+                  <>
+                    <span key={user_stat[0]} className="analytics_text">
+                      {user_stat[0]} sent {user_stat[1]} messages
+                    </span>
+                    <br />
+                  </>
+                );
+              })}
+            </div>
+          )}
+          {props.analytics.wordcount_stats && (
+            <div className="analytics_inner_div">
+              <Header as="h3">Top words used in channel:</Header>
+              {props.analytics.wordcount_stats.map((wordcount_stat) => {
+                return (
+                  <>
+                    <span key={wordcount_stat[0]} className="analytics_text">
+                      "{wordcount_stat[0]}" occurred {wordcount_stat[1]} times
+                    </span>
+                    <br />
+                  </>
+                );
+              })}
+            </div>
+          )}
+        </div>
+      ) : (
+        <>
+          <div id="chatMessages" className="chatMessages">
+            {(searchResults !== null &&
+              ((searchResults.length !== 0 &&
+                searchResults.map((message) => (
+                  <Message
+                    key={message.id}
+                    channelId={props.channelId}
+                    messageId={message.id}
+                    isFromCurrentUser={props.activeUserId === message.user_id}
+                    disableMessageDelete={true}
+                    displayName={message.display_name}
+                    timestamp={message.timestamp}
+                    messageContent={message.message_content}
+                    socket={props.socket}
+                  />
+                ))) || (
+                <Header as="h3">
+                  No results found for search query "{activeSearchQuery}"
+                </Header>
+              ))) ||
+              (props.messageList.length !== 0 &&
+                props.messageList.map((message) => {
                   return (
                     <Message
                       key={message.id}
-                      channelId={this.props.channelId}
+                      channelId={props.channelId}
                       messageId={message.id}
-                      isFromCurrentUser={this.props.activeUserId === message.user_id}
+                      isFromCurrentUser={props.activeUserId === message.user_id}
                       displayName={message.display_name}
                       timestamp={message.timestamp}
                       messageContent={message.message_content}
-                      socket={this.props.socket}
+                      socket={props.socket}
                     />
                   );
-                })}
-            </div>
-            <div className="chatInput">
-              <form onSubmit={(e) => this.props.handleSubmitMessage(e)}>
-                <input
-                  placeholder={"Send message to " + this.props.channelName}
-                  onChange={(event) => this.props.handleChange(event.target.value)}
-                  value={this.props.activeMessage}
-                />
-                <button type="submit" className="chatInputSubmit">
-                  send message
-                </button>
-              </form>
-            </div>
-          </>}
-      </div>
-    );
-  }
-}
+                }))}
+          </div>
+          <div className="chatInput">
+            <form onSubmit={(e) => props.handleSubmitMessage(e)}>
+              <input
+                disabled={!!activeSearchQuery}
+                placeholder={
+                  activeSearchQuery
+                    ? disabledSearchMessage
+                    : "Send message to " + props.channelName
+                }
+                onChange={(event) => props.handleChange(event.target.value)}
+                value={activeSearchQuery ? "" : props.activeMessage}
+              />
+              <button type="submit" className="chatInputSubmit">
+                send message
+              </button>
+            </form>
+          </div>
+        </>
+      )}
+    </div>
+  );
+};
 export default Chatroom;
