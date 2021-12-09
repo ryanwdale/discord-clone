a1a310bfaac4fd79ebbdf7158b46bc1dc9b438f4
Jonston Chan <jonstonc@sfu.ca>
2021-12-09 14:59:05 -0800

    Allow user to copy server invite link with one click

--- client/src/Invites/InviteModal.js
+++ client/src/Invites/InviteModal.js
@@ -1,16 +1,17 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { Button, Modal } from "semantic-ui-react";
+import { Button, Input, Modal } from "semantic-ui-react";
 import getCsrfCookie from "../Account/GetCsrfCookie";
-
-const createInviteUrl = (serverId, code) => `/join/${serverId}?code=${code}`;
-const createInviteLink = (url) => <a href={url}>invite link</a>;
 
 const InviteModal = (props) => {
   const [inviteCode, setInviteCode] = useState("");
-  const [inviteLink, setInviteLink] = useState("");
+  const [inviteUrl, setInviteUrl] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isCopied, setIsCopied] = useState(false);
+
+  const createInviteUrl = (serverId, code) =>
+    `${window.location.origin}/join/${serverId}?code=${code}`;
 
   const serverId = props.serverId;
 
@@ -20,21 +21,23 @@ const InviteModal = (props) => {
     }
 
     axios
-      .post(`/api/servers/${serverId}/invites`,
-      {},
-      {
-        headers: {
-          "Content-Type": "application/json",
-          "X-CSRF-TOKEN": getCsrfCookie()
-        },
-      })
+      .post(
+        `/api/servers/${serverId}/invites`,
+        {},
+        {
+          headers: {
+            "Content-Type": "application/json",
+            "X-CSRF-TOKEN": getCsrfCookie(),
+          },
+        }
+      )
       .then((v) => setInviteCode(v.data.code))
       .catch((e) => setErrorMessage(e.response.data.message));
   }, [isModalOpen, serverId]);
 
   useEffect(() => {
-    const inviteUrl = createInviteUrl(serverId, inviteCode);
-    setInviteLink(createInviteLink(inviteUrl));
+    setInviteUrl(createInviteUrl(serverId, inviteCode));
+    setIsCopied(false);
   }, [inviteCode, serverId]);
 
   return (
@@ -47,14 +50,33 @@ const InviteModal = (props) => {
         errorMessage ? (
           errorMessage
         ) : (
-          <Modal.Content>Your server {inviteLink}</Modal.Content>
+          <Modal.Content>
+            <p>
+              Share the following server invite link with your friends.
+              Note: this server link expires in 7 days
+            </p>
+            <Input
+              fluid
+              action={{
+                color: !isCopied ? "green" : "lightgrey",
+                labelPosition: "right",
+                icon: "copy",
+                content: !isCopied ? "Copy" : "Copied!",
+                onClick: () => {
+                  navigator.clipboard.writeText(inviteUrl);
+                  setIsCopied(true);
+                },
+              }}
+              value={inviteUrl}
+            />
+          </Modal.Content>
         )
       }
       actions={[
         {
           key: "done",
           content: errorMessage ? "Okay" : "I've copied the link!",
-          positive: true,
+          positive: isCopied,
         },
       ]}
     />
